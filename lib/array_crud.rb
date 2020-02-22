16c7ebbb7764d0a9f5633cd6c71f29f3570576e4
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-22 22:29:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -31,7 +31,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array_1 = ["wow", "I", "am", "really", "learning", "arrays!"]
+  array_1[0]
 end
 
 def retrieve_last_element_from_array(array)
