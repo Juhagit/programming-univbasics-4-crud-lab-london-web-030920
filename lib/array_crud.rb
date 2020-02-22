6b64af2fd99f7ffb71babd5eb3cbc7faa052fa59
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-22 22:19:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
  array_1 = ["wow", "hauskaa", "oppia", "uutta"]
- 
+ array_1 << "lontoo"
 end 
 def add_element_to_start_of_array(array, element)
   
