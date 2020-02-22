133f93fef8da2e27ff0712c91139352a79bfdeee
Juha Penttilä <juha.ea.penttila@gmail.com>
2020-02-22 22:23:59 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array_1 = ["wow", "hauskaa", "oppia", "uutta"]
-  array_1.pop
+  uutta_jooarray_1.pop
 end
 
 def remove_element_from_start_of_array(array)
